7b22c9dd8798ecc456ff4523a97dd115c0e70d34
Joris De Ridder <joris@ster.kuleuven.ac.be>
2013-01-24 13:54:58 +0100

    Expanded all tabs in 4 spaces. Removed spacing in front of final semi-colon. Added spacing in class public/private definitions.

--- include/FileProcess.h
+++ include/FileProcess.h
@@ -2,102 +2,101 @@
 // Enrico Corsaro @ IvS - 22 January 2013
 // e-mail: enrico.corsaro@ster,kuleuven.be
 // Header file "FileProcess.h"
+
 #include "lib.h"
 using namespace std;
 
 class FileProcess
 {
-	public:
-	// Public data members accessible from outside
-	unsigned long nlines;
-	int ncolumns;
-	vector<double> x;
-	vector<double> y;
-	
-	// Public member functions
-	// Class constructor. Requires the filename as input.
-	FileProcess (const char *name)
-	{
-		filename = name	;
-		nlines	= countLine()	;
-		ncolumns = countColumn()	;
-	}
-	
+    public:
+        unsigned long nlines;
+        int ncolumns;
+        vector<double> x;
+        vector<double> y;
+    
+        FileProcess (const char *name)
+        {
+            filename = name;
+            nlines  = countLine();
+            ncolumns = countColumn();
+        }
+    
 
-	// Read an ASCII file in a two columns format containing float numbers
-	// and save them in two vectors having double type, both accessible from outside
-	void read2ColDouble ()
-	{
-		ifstream inputFile		;
-		inputFile.open(filename)	;
-		
-		if (!inputFile)
-		{
-			cout << "Could not open file. Quitting program." << endl;
-			exit(1);
-		}
-	
-		x.resize(nlines);
-		y.resize(nlines);
-		
-		int i = 0;
-		for (int i=0; inputFile && (i < nlines); i++)
-		{
-			inputFile >> x.at(i) >> y.at(i) ;
-		}
-		//inputFile.close()		;
+        // Read an ASCII file in a two columns format containing float numbers
+        // and save them in two vectors having double type, both accessible from outside
 
-		return;
-	}
-	
-	
-	private:	
-	// Private member functions and data members
-	// Counts the total number of lines in a file
-	unsigned long countLine ()	
-	{
-		ifstream inputFile ;
-		inputFile.open(filename)	;
+        void read2ColDouble()
+        {
+            ifstream inputFile;
+            inputFile.open(filename);
+        
+            if (!inputFile)
+            {
+                cout << "Could not open file. Quitting program." << endl;
+                exit(1);
+            }
+    
+            x.resize(nlines);
+            y.resize(nlines);
+        
+            int i = 0;
+            for (int i=0; inputFile && (i < nlines); i++)
+            {
+                inputFile >> x.at(i) >> y.at(i);
+            }
+            //inputFile.close();
 
-		if (!inputFile)
-		{
-			cout << "Could not open file. Quitting program." << endl;
-			exit(1);
-		}
+            return;
+        }
+    
+    
+    private:    
+    
+        // Counts the total number of lines in a file
+        
+        unsigned long countLine ()  
+        {
+            ifstream inputFile;
+            inputFile.open(filename);
 
-		unsigned long n = 0	;
-		string s	;
-		while (getline(inputFile,s))
-			++n	;
+            if (!inputFile)
+            {
+                cout << "Could not open file. Quitting program." << endl;
+                exit(1);
+            }
 
-		inputFile.close()	;
-		return n		;
-	}
-	
-	// Counts the total number of columns in a file
-	int countColumn ()		
-	{
-		ifstream inputFile ;
-		inputFile.open(filename)	;
+            unsigned long n = 0;
+            string s;
+            while (getline(inputFile,s)) ++n;
 
-		if (!inputFile)
-		{
-			cout << "Could not open file. Quitting program." << endl;
-			exit(1);
-		}
+            inputFile.close();
+            return n;
+        }
+    
+        // Counts the total number of columns in a file
+        
+        int countColumn()      
+        {
+            ifstream inputFile;
+            inputFile.open(filename);
 
-		int ncol = 0			;
-		string line			;
-		getline(inputFile,line)		;
+            if (!inputFile)
+            {
+                cout << "Could not open file. Quitting program." << endl;
+                exit(1);
+            }
 
-		stringstream stringFile(line)	;
-		string ss			;
-		while (stringFile >> ss)
-			++ncol			;
+            int ncol = 0;
+            string line;
+            getline(inputFile,line);
 
-		inputFile.close()		;
-		return ncol			;
-	}
+            stringstream stringFile(line);
+            string ss;
+            while (stringFile >> ss) ++ncol;
 
-	const char *filename;
-}	;
+            inputFile.close();
+            return ncol;
+        }
+
+        const char *filename;
+};
