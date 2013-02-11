4af065cb4ccdb2affed2077d813fd61248643e2b
Joris De Ridder <joris@ster.kuleuven.ac.be>
2013-02-11 15:41:36 +0100

    Adapted MathExtra::logGaussProfile()  to work with Eigen arrays.

--- include/MathExtra.h
+++ include/MathExtra.h
@@ -14,25 +14,29 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
-
+#include <Eigen/Core>
 
 
 using namespace std;
 
+typedef Eigen::Ref<Eigen::ArrayXd> RefArrayXd;
+typedef Eigen::Ref<Eigen::ArrayXXd> RefArrayXXd;
+using Eigen::ArrayXd;
+using Eigen::ArrayXXd;
+
+
 namespace MathExtra
 {
+
     const double PI = 4.0 * atan(1.0);
     void lorentzProfile(vector<double> &y, vector<double> &x, double x0, double gamma, double amp = 1);
     double logGaussProfile(double x, double mu, double sigma, double amp = 1);
-    void logGaussProfile(vector<double> &y, vector<double> &x, double mu, double sigma, double amp = 1);
+    void logGaussProfile(RefArrayXd y, const ArrayXd x, const double mu, const double sigma, const double amp = 1);
     double gaussLikelihood(vector<double> &x_obs, vector<double> &x_theor, vector<double> &sigma);
     double logGaussLikelihood(vector<double> &x_obs, vector<double> &x_theor, vector<double> &sigma);
-
     inline double product(const vector<double> &vec);
     inline double sum(const vector<double> &vec);
     double logExpSum(double x, double y);
- 
-};
 
-
+}; // end namespace MathExtra
 #endif
