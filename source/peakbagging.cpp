0c2bf3df29151ae5ca7ec561565033c1d0bb0ee2
Joris De Ridder <joris@ster.kuleuven.ac.be>
2013-02-12 17:28:32 +0100

    Adapted peakbagging to the new version of File processing.

--- source/peakbagging.cpp
+++ source/peakbagging.cpp
@@ -3,15 +3,32 @@
 // e-mail: enrico.corsaro@ster.kuleuven.be
 // Source code file "peakbagging.cpp"
 
+#include <cstdlib>
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include "MathExtra.h"
-#include "FileProcess.h"
+#include "File.h"
 #include "NestedSampler.h"
 #include "NormalVariate.h"
 
-int main()
+
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        cerr << "Usage: peakbagging <outputfile>" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    ofstream outputFile(argv[1]);
+    if (!outputFile.good())
+    {
+        cerr << "Error opening output file" << endl;
+        exit(EXIT_FAILURE);
+    }
+       
+
     int Nobjects = 100;      // Number of objects per nested iteration (usually 100)
     int Niter    = 1000;     // Number of nested iterations (usually 1000)
     
@@ -20,17 +37,13 @@ int main()
     NestedSampler nestedSampler(normalVariate);
     nestedSampler.run(Nobjects, Niter);
 
-    cerr << right << setw(10) << "Parameter value" << right << setw(20) << "logLikelihood" << endl;
-    for (int i = 0; i < Niter; i++)
-    {
-       cout << right << setw(10) << nestedSampler.posteriorSample(i)
-            << right << setw(20) << nestedSampler.logLikelihoodOfPosteriorSample(i) 
-            << endl;
-    }
+    outputFile << "# Parameter value    logLikelihood" << endl;
+    outputFile << setiosflags(ios::fixed) << setprecision(8);
+    File::arrayToFile(outputFile, nestedSampler.posteriorSample.row(0), nestedSampler.logLikelihoodOfPosteriorSample);
+    outputFile.close();
     
-    cerr << endl;
-    cerr << " ------------------------------------------------" << endl;
     cerr << " Evidence: logZ = " << nestedSampler.getLogEvidence() << " +/- " << nestedSampler.getLogEvidenceError() << endl;
     cerr << " Information: H = " << nestedSampler.getInformationH() << endl;
-    cerr << " ------------------------------------------------" << endl;
+    
+    return EXIT_SUCCESS;
 }
