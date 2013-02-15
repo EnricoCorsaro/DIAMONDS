3c54f33b6d0b52d0adf82926099a483795b6c9bf
Joris De Ridder <joris@ster.kuleuven.ac.be>
2013-02-15 13:49:10 +0100

    Created a Likelihood abstract base class.

--- source/Likelihood.cpp
+++ source/Likelihood.cpp
@@ -1,36 +1,22 @@
+
 #include "Likelihood.h"
+
+
 
 // Likelihood::Likelihood()
 //
-// PURPOSE:
-//      Constructor. Reads input data file and chooses 
-//      the theoretical model to build the likelihood.
+// PURPOSE: constructor
 //
 // INPUT:
-//
-// OUTPUT:
+//      covariates:
+//      observations:
+//      uncertainties:
+//      model:
+// 
 
-Likelihood::Likelihood(const RefArrayXXd data, const int modelID)
-: modelIdentifier(modelID)
+Likelihood::Likelihood(RefArrayXd covariates, RefArrayXd observations, RefArrayXd uncertainties, Model &model);
+: covariates(covariates), observations(observations), uncertainties(uncertainties), model(model)
 {
-    int Ncols = 0;
-
-    Ncols = data.row(0).size();
-
-    if (Ncols > 3)
-    {
-        cerr << "The number of columns of the input data file exceeds the format.\nQuitting program." << endl;
-        exit(1);
-    }
-    else if (Ncols == 2)
-    {
-        covariates = data.col(0);
-        observations = data.col(1);
-        uncertainties = 0;
-    }
-    covariates = data.col(0);
-    observations = data.col(1);
-    uncertainties = data.col(2);
 
 }
 
@@ -38,174 +24,13 @@ Likelihood::Likelihood(const RefArrayXXd data, const int modelID)
 
 
 
+// Likelihood::~Likelihood()
+//
+// PURPOSE: destructor
+//
 
-// Likelihood::setLikelihood()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-void Likelihood::setLikelihood(const RefArrayXd modelParameters)
+Likelihood::~Likelihood()
 {
-    Model model(covariates, modelParameters, modelIdentifier);
-    predictions = model.getPredictions();
 
-    return;
 }
-
-
-
-
-
-
-
-// Likelihood::buildLogGaussianLikelihood()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-double Likelihood::buildLogGaussianLikelihood()
-{
-    double sumSigma = 0;
-
-    sumSigma = MathExtra::sum(uncertainties);
-    
-    if (sigmSigma == 0.)
-    {
-        cerr << "Gaussian Likelihood cannot be computed. Missing uncertainty information." 
-        << endl << "Quitting program." << endl;
-        exit(1);
-    }
-    
-
-    double likelihoodValue;
-    likelihoodValue = MathExtra::logGaussLikelihood(observations, predictions, uncertainties);
-
-    return likelihoodValue;
-}
-
-
-
-
-
-
-
-// Likelihood::buildMarginalizedLogGaussianLikelihood()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-double Likelihood::buildMarginalizedLogGaussianLikelihood()
-{
-    double likelihoodValue;
-
-    return likelihoodValue;
-}
-
-
-
-
-
-
-
-
-// Likelihood::buildMedianLikelihood()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-double Likelihood::buildMedianLikelihood()
-{
-    double likelihoodValue;
-
-    return likelihoodValue;
-}
-
-
-
-
-
-
-
-// Likelihood::getCovariates()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-ArrayXd Likelihood::getCovariates()
-{
-    return covariates;
-}
-
-
-
-
-
-
-// Likelihood::getObservations()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-ArrayXd Likelihood::getObservations()
-{
-    return observations;
-}
-
-
-
-
-
-
-
-// Likelihood::getUncertainties()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-ArrayXd Likelihood::getUncertainties()
-{
-    return uncertainties;
-}
-
-
-
-
-
-
-// Likelihood::getPredictions()
-//
-// PURPOSE:
-//
-// INPUT:
-//
-// OUTPUT:
-
-ArrayXd Likelihood::getPredictions()
-{
-    return predictions;
-}
-
-
-
 
