9781d08fea2b8e9e711ed47771e0904aa4529e30
Joris De Ridder <joris@ster.kuleuven.ac.be>
2013-02-15 13:50:33 +0100

    Created the Model abstract base class.

--- include/Model.h
+++ include/Model.h
@@ -1,43 +1,22 @@
-// Class for the computation of the
-// theoretical models.
-// Enrico Corsaro @ IvS - 14 February 2013
-// e-mail: enrico.corsaro@ster.kuleuven.be
-// Header file "Model.h"
-// Implementation contained in "Model.cpp"
 
 #ifndef MODEL_H
 #define MODEL_H
 
-#include <Eigen/Core>
-#include "MathExtra.h"
-
-using namespace std;
-using Eigen::ArrayXd;
-typedef Eigen::Ref<Eigen::ArrayXd> RefArrayXd;
-typedef Eigen::Ref<Eigen::ArrayXXd> RefArrayXXd;
 
 class Model
 {
-
     public:
-        
-        Model(const int modelID);
+    
+        Model();
         ~Model();
-        void setModel(const RefArrayXd x_obs, const RefArrayXd setOfParameters);
-        int getParametersNumber();
-        ArrayXd getYTheor();
-
-    private:
-
-        int modelIdentifier;
-        int parametersNumber;
-        ArrayXd x;
-        ArrayXd y_theor;
         
-        void buildModel1();
-        void buildModel2();
-        void buildModel3();
+        virtual void predict(RefArrayXd predictions, RefArrayXd parameters) = 0;
+        
+    protected:
+    
+    public:
+    
+};
 
-}; // END class Model
 
 #endif
