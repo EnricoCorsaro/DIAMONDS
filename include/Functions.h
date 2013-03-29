0fadb09b13a684dbdf41964d0559800dddd787e8
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2013-03-29 01:03:33 +0100

    Added new functions for covariance computation and decomposition and for hyper-shperical sampling distributions and Box-Muller normal distribution to Functions namespaces

--- include/Functions.h
+++ include/Functions.h
@@ -10,18 +10,22 @@
 #define FUNCTIONS_H
 
 #include <cmath>
+#include <assert>
 #include <numeric>
 #include <functional>
+#include <random>
+#include <ctime>
 #include <vector>
 #include <iostream>
 #include <cstdlib>
-#include <Eigen/Core>
+#include <Eigen/Dense>
+#include "EuclideanMetric.h"
 
 #define SWAP(a,b) {double copy; copy = a, a = b, b = copy;}
 
 
 using namespace std;
-using Eigen::ArrayXd;
+using namespace Eigen;
 typedef Eigen::Ref<Eigen::ArrayXd> RefArrayXd;
 
 
@@ -30,19 +34,38 @@ namespace Functions
 
     const double PI = 4.0 * atan(1.0);
 
+
     // Profile functions
+    
     void lorentzProfile(RefArrayXd predictions, const RefArrayXd covariates, const double centroid = 0.0, const double amplitude = 1.0, const double gamma = 1.0);
     double logGaussProfile(const double covariate, const double mu = 0.0, const double sigma = 1.0, const double amplitude = 1.0);
     void logGaussProfile(RefArrayXd y, const RefArrayXd x, const double mu = 0.0, const double sigma = 1.0, const double amplitude = 1.0);
     
+    
     // Likelihood functions
+    
     double logGaussLikelihood(const RefArrayXd observations, const RefArrayXd predictions, const RefArrayXd uncertainties);
     
-    // Array functions
+    
+    // Matrix algebra functions
+
+    void clusterCovariance(const RefArrayXXd clusterSample, RefArrayXXd covarianceMatrix, RefArrayXd centerCoordinates);
+    void covarianceDecomposition(const RefArrayXXd covarianceMatrix, RefArrayXd covarianceEigenvalues, RefArrayXXd covarianceEigenvectors);
+    
+
+    // Array algebra functions
+    
     inline double product(const vector<double> &vec);
     inline double sum(const vector<double> &vec);
     double logExpSum(double x, double y);
     void sortElements(RefArrayXd array1, RefArrayXd array2);
+
+
+    // Sampling Distributions
+
+    void hyperSphericalDistribution(RefArrayXXd sampleDistribution, const int Ndimensions, const int Npoints = 1, const double radius);
+    ArrayXXd BoxMullerDistribution(const int Npoints);
+
 
 } // END namespace Functions
 #endif
--- source/Functions.cpp
+++ source/Functions.cpp
@@ -139,6 +139,103 @@ double Functions::logGaussLikelihood(const RefArrayXd observations, const RefArr
 
 
 
+
+
+// Functions::clusterCovariance()
+//
+// PURPOSE:
+//      Compute the covariance matrix of a set of points belonging to a cluster.
+//
+// INPUT:
+//      clusterSample: an Eigen Array matrix of size (Ndimensions, Npoints)
+//      containing coordinates of all the points belonging to the cluster.
+//      covarianceMatrix: an Eigen Array matrix of size (Ndimensions, Ndimensions) where the
+//      covariance matrix is stored.
+//      centerCoordinates: an Eigen Array of size Ndimensions where the coordinates of the
+//      center (mean values) of the cluster of points are stored.
+//
+// OUTPUT:
+//      void
+//
+
+void Functions::clusterCovariance(const RefArrayXXd clusterSample, RefArrayXXd covarianceMatrix, RefArrayXd centerCoordinates)
+{    
+    assert(covarianceMatrix.cols() == covarianceMatrix.rows());
+    
+    int Ndimensions = clusterSample.rows();
+    int Npoints = clusterSample.cols();
+    covarianceMatrix.resize(Ndimensions, Ndimensions);
+    centerCoordinates.resize(Ndimensions);
+
+    for (int i=0; i < Ndimensions; i++)
+    {
+        centerCoordinates(i) = (clusterSample.row(i).sum())/Npoints;
+    }
+
+    double biasFactor = 1./(Npoints-1);
+
+    for (int i=0; i < Ndimensions; i++)
+    {
+        for (int j=0; j < Ndimensions; j++)
+        {
+            covarianceMatrix(i,j) = biasFactor * ((clusterSample.row(i) - centerCoordinates(i))*(clusterSample.row(j) - centerCoordinates(j))).sum();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Functions::covarianceDecomposition()
+//
+// PURPOSE:
+//      Compute the decomposition of a covariance matrix into eigenvectors and eigenvalues.
+//
+// INPUT:
+//      covarianceMatrix: an Eigen Array matrix to be decomposed
+//      eigenValues: an Eigen Array to contain the eigenvalues 
+//      of the covariance matrix
+//      eigenVectorsMatrix: an Eigen Array matrix to contain the matrix of
+//      the eigenvectors of the covariance matrix
+//
+// OUTPUT:
+//      void
+//
+
+void Functions::covarianceDecomposition(const RefArrayXXd covarianceMatrix, RefArrayXd eigenValues, RefArrayXXd eigenVectorsMatrix)
+{
+    assert(covarianceMatrix.cols() == covarianceMatrix.rows());
+
+    int Ndimensions = covarianceMatrix.cols();
+
+    SelfAdjointEigenSolver<MatrixXd> eigenSolver(covarianceMatrix.matrix());
+
+    if (eigenSolver.info() != Success) abort();
+
+    eigenValues.resize(Ndimensions)
+    eigenVectorsMatrix.resize(Ndimensions,Ndimensions)
+
+    eigenValues = eigenSolver.eigenvalues();
+    eigenVectorsMatrix = eigenSolver.eigenvectors();
+}
+
+
+
+
+
+
+
+
+
+
 // Functions::product()
 //
 // PURPOSE: 
@@ -250,3 +347,103 @@ void Functions::sortElements(RefArrayXd array1, RefArrayXd array2)
     }
     
 } // END Functions::sortElements()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Functions::hyperSphericalDistribution()
+//
+// PURPOSE: 
+//      Generate a sample of points from a N-dimensional hyper-sphere of given
+//      radius that is uniformly distributed in its volume. The method
+//      follows the approach described in Shaw J. R et al. (2007; MNRAS, 378, 1365).
+//
+// INPUT: 
+//      sampleDistribution: an Eigen Array of size (Ndimensions, Npoints) to contain the
+//      points spherically distributed.
+//      Ndimensions: the dimensions of the hyper-sphere
+//      Npoints: the number of points of the distribution
+//      radius: the radius of the hyper-sphere
+//
+// OUTPUT: 
+//      void
+//
+
+void Functions::hyperSphericalDistribution(RefArrayXXd sampleDistribution, const int Ndimensions, const int Npoints = 1, const double radius)
+{
+    mt19937 engine(time(0));
+    uniform_real_distribution<double> uniform_dist(0.0,1.0);
+    auto uniform = bind(uniform_dist, engine);
+
+    normal_distribution<double> normal_dist(0.0,1.0);
+    auto normal = bind(normal_dist, engine);
+
+    sampleDistribution.resize(Ndimensions, Npoints);
+
+    for (int i = 0; i < Npoints; i++)
+    {
+        for (int j = 0; j < Ndimensions; j++)
+        {
+            sampleDistribution(j,i) = normal(); 
+        }
+        sampleDistribution.col(i) = uniform()*radius*sampleDistribution.col(i)/sqrt(sampleDistribution.col(i).square().sum());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Functions::BoxMullerDistribution()
+//
+// PURPOSE: 
+//      Generates a 2D normal distribution from a uniform distribution by
+//      means of the Box-Muller algorithm.
+//
+// INPUT:
+//      Npoints: number of samples in the distribution.
+//
+// OUTPUT:
+//      An Eigen Array containing the values of the distribution.
+//
+// REMARKS:
+//      THe normal distribution generated has zero mean and unit standard deviation.
+//
+
+ArrayXXd Functions::BoxMullerDistribution(const int Npoints)
+{
+    mt19937 engine(time(0));
+    uniform_real_distribution<double> uniform_dist(0.0,1.0);
+    auto uniform = bind(uniform_dist, engine);
+
+    ArrayXd u;
+    ArrayXd v;
+    ArrayXXd bm(2, Npoints);
+
+    for (int i = 0; i < Npoints; i++)
+    {
+        u(i) = uniform();
+        v(i) = uniform();
+        bm(0,i) = sqrt(-2*log(u(i))) * cos(2*PI*v(i));
+        bm(1,i) = sqrt(-2*log(u(i))) * sin(2*PI*v(i));
+    }
+
+    return bm;
+}
