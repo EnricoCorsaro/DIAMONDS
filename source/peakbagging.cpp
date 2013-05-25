63ed698f2daeb4b4f874ecdbceff6fc561fd12df
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2013-05-25 19:51:36 +0200

    Updated peakbagging.cpp according to new ellipsoidal sampling method.

--- source/peakbagging.cpp
+++ source/peakbagging.cpp
@@ -9,13 +9,16 @@
 #include <fstream>
 #include "Functions.h"
 #include "File.h"
-#include "NestedSampler.h"
-
+#include "MultiEllipsoidSampler.h"
+#include "KmeansClusterer.h"
+#include "EuclideanMetric.h"
+#include "Prior.h"
 #include "UniformPrior.h"
+#include "NormalPrior.h"
 #include "NormalLikelihood.h"
 #include "LorentzianModel.h"
 #include "Results.h"
-
+#include "Ellipsoid.h"
 
 int main(int argc, char *argv[])
 {
@@ -60,17 +63,23 @@ int main(int argc, char *argv[])
     int Ndimensions = 3;        // Number of free parameters (dimensions) of the problem
 
 
-    // Define boundaries of the free parameters of the problem (should be done with separate routine)
+    // First step - Setting Prior distribution and parameter space
 
     ArrayXd parametersMinima(Ndimensions);
     ArrayXd parametersMaxima(Ndimensions);
     parametersMinima <<  0.0, 0.8, 1.0;         // Centroid, Amplitude, Gamma
     parametersMaxima << 20.0, 1.5, 3.0;
-    
-    
-    // First step - Setting Prior distribution and parameter space
 
-    UniformPrior prior(parametersMinima, parametersMaxima);
+    //ArrayXd parametersMean(Ndimensions);
+    //ArrayXd parametersSDV(Ndimensions);
+    //parametersMean << 9.2,1.2,2.0;
+    //parametersSDV << 0.5,0.2,0.2;
+    
+    vector<Prior*> ptrPriorsVector(1);
+    UniformPrior uniformPrior(parametersMinima, parametersMaxima);
+    ptrPriorsVector[0] = &uniformPrior;
+    //NormalPrior normalPrior(parametersMean, parametersSDV);
+    //ptrPriorsVector[0] = &normalPrior;
 
 
     // Second step - Setting up a model for the inference problem
@@ -83,13 +92,31 @@ int main(int argc, char *argv[])
     NormalLikelihood likelihood(covariates, observations, uncertainties, model);
     
 
-    // Fourth step - Starting nested sampling process
+    // Fourth step - Set up the K-means clusterer using an Euclidean metric
+
+    EuclideanMetric myMetric;
+    int minNclusters = 2;
+    int maxNclusters = 10;
+    int Ntrials = 10;
+    double relTolerance = 0.01;
+
+    KmeansClusterer kmeans(myMetric, minNclusters, maxNclusters, Ntrials, relTolerance); 
+
+
+    // Fifth step - Start nested sampling process
     
-    NestedSampler nestedSampler(prior, likelihood);
-    nestedSampler.run(Nobjects);
+    int Ndraws = 1;
+    int Nobjects = 1000;
+    int NiterationsBeforeClustering = 2;        // Number of nesting iterations before executing clustering algorithm again
+    double initialEnlargementFactor = 1.2;  
+    double alpha = 1.0;                         // Exponent for remaining prior mass in ellipsoid enlargement factor
+    double terminationFactor = 0.5;             // Termination factor for nesting loop
+
+    MultiEllipsoidSampler nestedSampler(ptrPriorsVector, likelihood, myMetric, kmeans, Nobjects, initialEnlargementFactor, alpha);
+    nestedSampler.run(terminationFactor, NiterationsBeforeClustering);
 
 
-    // Save the results in an output file
+    // Save the results in output files
 
     Results results(nestedSampler);
     string outputDirName(argv[2]);
@@ -97,7 +124,7 @@ int main(int argc, char *argv[])
     results.writeLogLikelihoodToFile(outputDirName + "/likelihood.txt");
     results.writeEvidenceInformationToFile(outputDirName + "/evidence.txt");
     results.writePosteriorProbabilityToFile(outputDirName + "/posterior.txt");
-    results.writeParameterEstimationToFile(outputDirName + "/parameterestimation.txt");
+    results.writeParameterEstimationToFile(outputDirName + "/parameters_estimation.txt");
     
     return EXIT_SUCCESS;
 }
