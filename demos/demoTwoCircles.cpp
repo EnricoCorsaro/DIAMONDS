14f2bbc0374276c9915768bc76b27e79c0a19d5d
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2013-10-02 21:04:19 +0200

    Updated configuring parameters for demoTwoCircles. Demo now runs correctly and is able to compute good evidence value.

--- demos/demoTwoCircles.cpp
+++ demos/demoTwoCircles.cpp
@@ -40,8 +40,8 @@ int main(int argc, char *argv[])
     vector<Prior*> ptrPriors(1);              // One prior, covering both coordinates
     ArrayXd parametersMinima(Ndimensions);
     ArrayXd parametersMaxima(Ndimensions);
-    parametersMinima << -7.0, -4.0;         // Centroid x direction, Centroid y direction
-    parametersMaxima << +7.0, +4.0;
+    parametersMinima << -7.0, -6.0;         // Centroid x direction, Centroid y direction
+    parametersMaxima << +7.0, +6.0;
     UniformPrior uniformPrior(parametersMinima, parametersMaxima);
     ptrPriors[0] = &uniformPrior;
     
@@ -70,13 +70,13 @@ int main(int argc, char *argv[])
 
     // Start nested sampling process
     bool printOnTheScreen = true;                   // Print results on the screen
-    int Nobjects = 400;                             // Number of active points evolving within the nested sampling process. 
-    int maxNdrawAttempts = 200;                    // Maximum number of attempts when trying to draw a new sampling point.
-    int NinitialIterationsWithoutClustering = 100;  // The first N iterations, we assume that there is only 1 cluster.
+    int Nobjects = 500;                             // Number of active points evolving within the nested sampling process. 
+    int maxNdrawAttempts = 20000;                    // Maximum number of attempts when trying to draw a new sampling point.
+    int NinitialIterationsWithoutClustering = 300;  // The first N iterations, we assume that there is only 1 cluster.
     int NiterationsWithSameClustering = 10;         // Clustering is only happening every X iterations.
-    double initialEnlargementFraction = 2.5;        // Fraction by which each axis in an ellipsoid has to be enlarged.
+    double initialEnlargementFraction = 5.0;        // Fraction by which each axis in an ellipsoid has to be enlarged.
                                                     // It can be a number >= 0, where 0 means no enlargement.
-    double shrinkingRate = 0.5;                     // Exponent for remaining prior mass in ellipsoid enlargement fraction.
+    double shrinkingRate = 0.2;                     // Exponent for remaining prior mass in ellipsoid enlargement fraction.
                                                     // It is a number between 0 and 1. The smaller the slower the shrinkage
                                                     // of the ellipsoids.
     double terminationFactor = 0.01;                // Termination factor for nesting loop.
