d498fb29078ae4d8144a787987b2ddd69feca8c5
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2013-10-08 16:40:44 +0200

    Included demoSingleNDGaussian for testing MultiNest with a Ndimensional Gaussian. Centroids are generated randomly within the range [-10,10] while sigmas are generated within the range [0.5,1.0]. Number of dimensions are to be defined in the main function.

---
+++ demos/demoSingleNDGaussian.cpp
@@ -0,0 +1,106 @@
+//
+// Compile with: clang++ -o demoSingleNDGaussian demoSingleNDGaussian.cpp -L../build/ -I ../include/ -l multinest -stdlib=libc++ -std=c++11
+// 
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Functions.h"
+#include "File.h"
+#include "MultiEllipsoidSampler.h"
+#include "KmeansClusterer.h"
+#include "EuclideanMetric.h"
+#include "Prior.h"
+#include "UniformPrior.h"
+#include "NormalPrior.h"
+#include "Results.h"
+#include "Ellipsoid.h"
+#include "ZeroModel.h"
+#include "demoSingleNDGaussian.h"
+
+
+
+int main(int argc, char *argv[])
+{
+    unsigned long Nrows;
+    int Ncols;
+    ArrayXXd data;
+
+  
+    // Creating dummy arrays for the covariates and the observations.
+    // They're not used because we compute our Likelihood directly. 
+
+    ArrayXd covariates;
+    ArrayXd observations;
+
+
+    // Setting Prior distribution and parameter space
+
+    int Ndimensions = 4;        // Number of free parameters (dimensions) of the problem
+    vector<Prior*> ptrPriors(1);
+    ArrayXd parametersMinima(Ndimensions);
+    ArrayXd parametersMaxima(Ndimensions);
+    parametersMinima.fill(-20);         
+    parametersMaxima.fill(20);
+    UniformPrior uniformPrior(parametersMinima, parametersMaxima);
+    ptrPriors[0] = &uniformPrior;
+    
+
+    // Set up a dummy model. This won't be used because we're computing
+    // the Likelihood directly, but the Likelihood nevertheless expects a model in 
+    // its constructor.
+    
+    ZeroModel model(covariates);
+
+
+    // Set up the likelihood function to be used
+    
+    SingleNDGaussianLikelihood likelihood(observations, model, Ndimensions);
+
+
+    // Set up the K-means clusterer using an Euclidean metric
+
+    EuclideanMetric myMetric;
+    int minNclusters = 1;
+    int maxNclusters = 10;
+    int Ntrials = 10;
+    double relTolerance = 0.01;
+
+    KmeansClusterer kmeans(myMetric, minNclusters, maxNclusters, Ntrials, relTolerance); 
+
+
+    // Start nested sampling process
+    
+    bool printOnTheScreen = true;                   // Print results on the screen
+    int Nobjects = 1500;                             // Number of active points evolving within the nested sampling process. 
+    int maxNdrawAttempts = 1000;                     // Maximum number of attempts when trying to draw a new sampling point.
+    int NinitialIterationsWithoutClustering = 10;  // The first N iterations, we assume that there is only 1 cluster.
+    int NiterationsWithSameClustering = 10;         // Clustering is only happening every X iterations.
+    double initialEnlargementFraction = 2.0;        // Fraction by which each axis in an ellipsoid has to be enlarged.
+                                                    // It can be a number >= 0, where 0 means no enlargement.
+    double shrinkingRate = 0.8;                     // Exponent for remaining prior mass in ellipsoid enlargement fraction.
+                                                    // It is a number between 0 and 1. The smaller the slower the shrinkage
+                                                    // of the ellipsoids.
+    double terminationFactor = 0.01;                // Termination factor for nesting loop.
+
+    
+    MultiEllipsoidSampler nestedSampler(printOnTheScreen, ptrPriors, likelihood, myMetric, kmeans, 
+                                        Nobjects, initialEnlargementFraction, shrinkingRate);
+    nestedSampler.run(terminationFactor, NinitialIterationsWithoutClustering, NiterationsWithSameClustering, maxNdrawAttempts);
+
+
+    // Save the results in output files
+
+    Results results(nestedSampler);
+    results.writeParametersToFile("demoSingleNDGaussian_Parameter");
+    results.writeLogLikelihoodToFile("demoSingleNDGaussian_LogLikelihood.txt");
+    results.writeEvidenceInformationToFile("demoSingleNDGaussian_Evidence.txt");
+    results.writePosteriorProbabilityToFile("demoSingleNDGaussian_Posterior.txt");
+    results.writeParametersSummaryToFile("demoSingleNDGaussian_ParametersSummary.txt");
+
+
+    // That's it!
+
+    return EXIT_SUCCESS;
+}
---
+++ demos/demoSingleNDGaussian.h
@@ -0,0 +1,127 @@
+
+#ifndef SINGLENDGAUSSIANLIKELIHOOD_H
+#define SINGLENDGAUSSIANLIKELIHOOD_H
+
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+#include <functional>
+#include "Functions.h"
+#include <Eigen/Core>
+#include "Likelihood.h"
+
+
+using namespace std;
+using Eigen::ArrayXd;
+typedef Eigen::Ref<Eigen::ArrayXd> RefArrayXd;
+
+
+// Class definition
+
+class SingleNDGaussianLikelihood : public Likelihood
+{
+
+    public:
+
+        SingleNDGaussianLikelihood(const RefArrayXd observations, Model &model,int Ndimensions);
+        ~SingleNDGaussianLikelihood();
+
+        virtual double logValue(RefArrayXd nestedSampleOfParameters);
+
+
+    private:
+    
+        int Ndimensions;
+        ArrayXd centroid;
+        ArrayXd sigma;
+
+}; 
+
+
+
+
+// SingleNDGaussianLikelihood::SingleNDGaussianLikelihood()
+//
+// PURPOSE: 
+//      Derived class constructor.
+//
+// INPUT:
+// 
+
+SingleNDGaussianLikelihood::SingleNDGaussianLikelihood(const RefArrayXd observations, Model &model, int Ndimensions)
+: Likelihood(observations, model),
+  Ndimensions(Ndimensions)
+{
+    centroid.resize(Ndimensions);
+    sigma.resize(Ndimensions);
+
+    mt19937 engine;
+    clock_t clockticks = clock();
+    engine.seed(clockticks);
+
+    uniform_real_distribution<double> uniform1(0.5,1.0);
+    uniform_real_distribution<double> uniform2(-10.0,10.0);
+
+    cout << "Gaussian Likelihood in " << Ndimensions << " dimensions" << endl;
+
+    for (int i=0; i < Ndimensions; ++i)
+    {
+        centroid(i) = uniform2(engine);
+        sigma(i) = uniform1(engine);
+        cout << "Dimension #: " << i << "   " << "Centroid: " << centroid(i) << "   " << "sigma: " << sigma(i) << endl;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+// SingleNDGaussianLikelihood::~SingleNDGaussianLikelihood()
+//
+// PURPOSE: 
+//      Derived class destructor.
+//
+
+SingleNDGaussianLikelihood::~SingleNDGaussianLikelihood()
+{
+}
+
+
+
+
+
+
+
+
+// SingleNDGaussianLikelihood::logValue()
+//
+// PURPOSE:
+//      Compute a Toy Likelihood example: Ndiemsional Gaussian
+//
+// INPUT:
+//      nestedSampleOfParameters: a one-dimensional array containing the actual
+//                                values of the free parameters that describe the model.
+//
+// OUTPUT:
+//      a double number containing the log-likelihood value.
+//
+
+double SingleNDGaussianLikelihood::logValue(RefArrayXd nestedSampleOfParameters)
+{
+    assert(nestedSampleOfParameters.size() == Ndimensions);
+    ArrayXd exponent(Ndimensions);
+   
+    exponent = ((nestedSampleOfParameters - centroid)/sigma).square();
+    
+    double logTotalLikelihood = -log(2*Functions::PI*sigma.prod()) -0.5*exponent.sum();
+    return logTotalLikelihood;
+}
+
+
+#endif
