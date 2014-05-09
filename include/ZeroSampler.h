5286eb510e42a6c714076cf32267e4a97e7c91ba
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2014-05-09 18:48:30 +0200

    Added ZeroSampler class for building empty nested sampler objects. To be used when merging the results from different runs.

---
+++ include/ZeroSampler.h
@@ -0,0 +1,35 @@
+// Derived class for building empty nested sampler objects.
+// Created by Enrico Corsaro @ IvS - 9 May 2014
+// e-mail: enrico.corsaro@ster.kuleuven.be
+// Header file "ZeroSampler.h"
+// Implementation contained in "ZeroSampler.cpp"
+
+#ifndef ZEROSAMPLER_H
+#define ZEROSAMPLER_H
+
+#include <algorithm>
+#include <Eigen/Dense>
+#include "NestedSampler.h"
+
+using namespace std;
+
+class ZeroSampler : public NestedSampler
+{
+
+    public:
+       
+        ZeroSampler(const bool printOnTheScreen, const int initialNobjects, const int minNobjects, vector<Prior*> ptrPriors, 
+                    Likelihood &likelihood, Metric &metric, Clusterer &clusterer); 
+        ~ZeroSampler();
+
+        virtual bool drawWithConstraint(const RefArrayXXd totalSample, const unsigned int Nclusters, const vector<int> &clusterIndices,
+                                        const vector<int> &clusterSizes, RefArrayXd drawnPoint, 
+                                        double &logLikelihoodOfDrawnPoint, const int maxNdrawAttempts) override; 
+        
+    protected:
+      
+    private:
+
+};
+
+#endif
---
+++ source/ZeroSampler.cpp
@@ -0,0 +1,83 @@
+#include "ZeroSampler.h"
+
+// ZeroSampler::ZeroSampler()
+//
+// PURPOSE: 
+//      Constructor. Sets initial information, logEvidence and type 
+//      of prior and likelihood distributions to be used. 
+//
+// INPUT:
+//      printOnTheScreen:       Boolean value specifying whether the results are to 
+//                              be printed on the screen or not.
+//      initialNobjects:        Initial number of live points to start the nesting process
+//      minNobjects:            Minimum number of live points allowed in the nesting process
+//      ptrPriors:              Vector of pointers to Prior class objects
+//      likelihood:             Likelihood class object used for likelihood sampling.
+//      metric:                 Metric class object to contain the metric used in the problem.
+//      clusterer:              Clusterer class object specifying the type of clustering algorithm to be used.
+//
+
+ZeroSampler::ZeroSampler(const bool printOnTheScreen, const int initialNobjects, const int minNobjects, vector<Prior*> ptrPriors, 
+                    Likelihood &likelihood, Metric &metric, Clusterer &clusterer)
+: NestedSampler(printOnTheScreen, initialNobjects, minNobjects, ptrPriors, likelihood, metric, clusterer)
+{
+}
+
+
+
+
+
+
+
+
+
+
+// ZeroSampler::~ZeroSampler()
+//
+// PURPOSE: 
+//      Base class destructor.
+//
+
+ZeroSampler::~ZeroSampler()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// ZeroSampler::drawWithConstraint()
+//
+// PURPOSE:
+//      Empty function not to be used.
+//
+// INPUT:
+//      totalSample:                Eigen Array matrix of size (Ndimensions, Nobjects)
+//                                  containing the total sample of active points at a given nesting iteration
+//      Nclusters:                  Optimal number of clusters found by clustering algorithm
+//      clusterIndices:             Indices of clusters for each point of the sample
+//      clusterSizes:               A vector of integers containing the number of points belonging to each cluster
+//      drawnPoint:                 Eigen Array matrix of size (Ndimensions,Ndraws) to contain the
+//                                  coordinates of the drawn point to be used for the next nesting loop. 
+//                                  When used for the first time, the array contains the coordinates of the 
+//                                  worst nested object to be updated.
+//      maxNdrawAttempts:           Maximum number of attempts allowed when drawing from a single ellipsoid.
+//      
+//
+// OUTPUT:
+//      A boolean value that is true if a new point in the sampling process is found and false otherwise.
+//
+
+bool ZeroSampler::drawWithConstraint(const RefArrayXXd totalSample, const unsigned int Nclusters, const vector<int> &clusterIndices,
+                                               const vector<int> &clusterSizes, RefArrayXd drawnPoint, 
+                                               double &logLikelihoodOfDrawnPoint, const int maxNdrawAttempts)
+{    
+    return false;
+}
